f8d6653f2d29b63b4bec530e343e12345a23846a
salvatore sorrentino <salvatore.sorrentino@live.com>
2024-01-24 08:27:56 +0100

    minor fix

--- HelloMVC/Controllers/EventiController.cs
+++ HelloMVC/Controllers/EventiController.cs
@@ -16,14 +16,4 @@ public class EventiController : Controller
     {
         return View(eventi.EstraiEventi());
     }
-
-    //private List<Evento> EstraiEventi() { 
-    // return new List<Evento>
-    // {
-    //    new Evento { Id = 1, Data = new DateTime(2021, 10, 10), Titolo = "Festa della birra"},
-    //    new Evento { Id = 2, Data = new DateTime(2021, 11, 10), Titolo = "Festa del vino" },
-    //    new Evento { Id = 3, Data = new DateTime(2021, 12, 10), Titolo = "Festa dell'amaro" },
-    // };
-    //}
-
 }
--- HelloMVC/Program.cs
+++ HelloMVC/Program.cs
@@ -1,5 +1,3 @@
-using Microsoft.Extensions.FileSystemGlobbing.Internal.Patterns;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
